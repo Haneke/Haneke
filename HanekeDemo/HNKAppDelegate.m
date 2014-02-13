d6ff483dd996d86e8dc95331fd875c23befa3eca
hpique <hermes.pique@robotmedia.net>
2014-02-13 17:27:23 +0100

    Move all logic to HNKViewController

--- HanekeDemo/HNKAppDelegate.m
+++ HanekeDemo/HNKAppDelegate.m
@@ -15,51 +15,10 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
-    [self prepareImages];
     HNKViewController *vc = [HNKViewController new];
     self.window.rootViewController = vc;
     [self.window makeKeyAndVisible];
     return YES;
-}
-
-- (void)prepareImages
-{
-    NSString *documents = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
-    for (NSUInteger i = 0; i < 100; i++)
-    {
-        @autoreleasepool {
-            NSLog(@"Creating image %ld of %d", (long)i + 1, 100);
-            UIImage *image = [self imageWithIndex:i];
-            NSData *data = UIImageJPEGRepresentation(image, 1);
-            NSString *fileName = [NSString stringWithFormat:@"sample%ld.jpg", (long)i];
-            NSString *path = [documents stringByAppendingPathComponent:fileName];
-            [data writeToFile:path atomically:YES];
-        }
-    }
-}
-
-#pragma mark - Utils
-
-- (UIImage*)imageWithIndex:(NSUInteger)index
-{
-    // Photo by Paul Sableman, taken from http://www.flickr.com/photos/pasa/8636568094
-    UIImage *sample = [UIImage imageNamed:@"sample.jpg"];
-    NSString *indexString = [NSString stringWithFormat:@"%ld", (long)index];
-    return [self drawText:indexString inImage:sample];
-}
-
-- (UIImage*)drawText:(NSString*)text inImage:(UIImage*)image
-{
-    UIFont *font = [UIFont boldSystemFontOfSize:image.size.height / 2];
-    UIGraphicsBeginImageContext(image.size);
-    [image drawInRect:CGRectMake(0,0,image.size.width,image.size.height)];
-    NSDictionary *attributes = @{NSFontAttributeName : font, NSForegroundColorAttributeName : [UIColor whiteColor]};
-    CGSize textSize = [text sizeWithAttributes:attributes];
-    CGRect rect = CGRectMake((image.size.width - textSize.width) / 2, (image.size.height - textSize.height) / 2, textSize.width, textSize.height);
-    [text drawInRect:rect withAttributes:attributes];
-    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    return newImage;
 }
 
 @end
--- HanekeDemo/HNKViewController.m
+++ HanekeDemo/HNKViewController.m
@@ -9,11 +9,21 @@
 #import "HNKViewController.h"
 #import "UIImageView+Haneke.h"
 
-#define HNK_USE_CUSTOM_FORMAT 0
+#define HNK_USE_CUSTOM_FORMAT 1
 
 @implementation HNKViewController {
-    NSMutableArray *_items;
-    HNKCacheFormat *_customFormat;
+    NSArray *_items;
+}
+
++ (void)initialize
+{
+    HNKCacheFormat *format = [[HNKCacheFormat alloc] initWithName:@"thumbnail"];
+    format.diskCapacity = 0.1 * 1024 * 1024;
+    format.compressionQuality = 0.5;
+    format.size = CGSizeMake(100, 100);
+    format.scaleMode = HNKScaleModeAspectFill;
+    format.preloadPolicy = HNKPreloadPolicyLastSession;
+    [[HNKCache sharedCache] registerFormat:format];
 }
 
 - (void)viewDidLoad
@@ -21,13 +31,6 @@
     [super viewDidLoad];
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"Cell"];
     [self initializeItems];
-    _customFormat = [[HNKCacheFormat alloc] initWithName:@"thumbnail"];
-    _customFormat.diskCapacity = 1 * 1024 * 1024;
-    _customFormat.compressionQuality = 0.5;
-    _customFormat.size = CGSizeMake(100, 100);
-    _customFormat.scaleMode = HNKScaleModeAspectFill;
-    _customFormat.preloadPolicy = HNKPreloadPolicyLastSession;
-    [[HNKCache sharedCache] registerFormat:_customFormat];
 }
 
 #pragma mark - Table view data source
@@ -46,7 +49,7 @@
     cell.imageView.image = [UIImage imageNamed:@"placeholder"];
     if (HNK_USE_CUSTOM_FORMAT)
     {
-        cell.imageView.hnk_cacheFormat = _customFormat;
+        cell.imageView.hnk_cacheFormat = [HNKCache sharedCache].formats[@"thumbnail"];
     }
     else
     { // Resize image based on the `bounds` and `contentMode` of the `UIImageView`, using a default configuration
@@ -61,14 +64,49 @@
 
 - (void)initializeItems
 {
-    _items = [NSMutableArray array];
-    NSString *documents = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
-    for (int i = 0; i < 100; i++)
-    {
-        NSString *fileName = [NSString stringWithFormat:@"sample%ld.jpg", (long)i];
-        NSString *path = [documents stringByAppendingPathComponent:fileName];
-        [_items addObject:path];
-    }
+    _items = [NSArray array];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSString *documents = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+        NSMutableArray *items = [NSMutableArray array];
+        for (NSUInteger i = 0; i < 100; i++)
+        {
+            @autoreleasepool {
+                NSLog(@"Creating image %ld of %d", (long)i + 1, 100);
+                UIImage *image = [self imageWithIndex:i];
+                NSData *data = UIImageJPEGRepresentation(image, 1);
+                NSString *fileName = [NSString stringWithFormat:@"sample%ld.jpg", (long)i];
+                NSString *path = [documents stringByAppendingPathComponent:fileName];
+                [data writeToFile:path atomically:YES];
+                [items addObject:path];
+            }
+        }
+        dispatch_sync(dispatch_get_main_queue(), ^{
+            _items = items;
+            [self.tableView reloadData];
+        });
+    });
+}
+
+- (UIImage*)imageWithIndex:(NSUInteger)index
+{
+    // Photo by Paul Sableman, taken from http://www.flickr.com/photos/pasa/8636568094
+    UIImage *sample = [UIImage imageNamed:@"sample.jpg"];
+    NSString *indexString = [NSString stringWithFormat:@"%ld", (long)index];
+    return [self drawText:indexString inImage:sample];
+}
+
+- (UIImage*)drawText:(NSString*)text inImage:(UIImage*)image
+{
+    UIFont *font = [UIFont boldSystemFontOfSize:image.size.height / 2];
+    UIGraphicsBeginImageContext(image.size);
+    [image drawInRect:CGRectMake(0,0,image.size.width,image.size.height)];
+    NSDictionary *attributes = @{NSFontAttributeName : font, NSForegroundColorAttributeName : [UIColor whiteColor]};
+    CGSize textSize = [text sizeWithAttributes:attributes];
+    CGRect rect = CGRectMake((image.size.width - textSize.width) / 2, (image.size.height - textSize.height) / 2, textSize.width, textSize.height);
+    [text drawInRect:rect withAttributes:attributes];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
 }
 
 @end
