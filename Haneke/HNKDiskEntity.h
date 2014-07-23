b1dd3f1525768c2ee04513d899209fbd662b53a9
Hermes Pique <hermespique@gmail.com>
2014-07-23 20:14:53 +0200

    HNKDiskEntity

---
+++ Haneke/HNKDiskEntity.h
@@ -0,0 +1,13 @@
+//
+//  HNKDiskEntity.h
+//  Haneke
+//
+//  Created by Hermes Pique on 7/23/14.
+//  Copyright (c) 2014 Hermes Pique. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HNKDiskEntity : NSObject
+
+@end
---
+++ Haneke/HNKDiskEntity.m
@@ -0,0 +1,13 @@
+//
+//  HNKDiskEntity.m
+//  Haneke
+//
+//  Created by Hermes Pique on 7/23/14.
+//  Copyright (c) 2014 Hermes Pique. All rights reserved.
+//
+
+#import "HNKDiskEntity.h"
+
+@implementation HNKDiskEntity
+
+@end
